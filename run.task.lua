58febbbd4c3c6ac0ad7ac3b7015a03eb3f4fb31e
Pt <pt.main.acc@gmail.com>
2026-09-11 17:36:25 +0400

    Update run.task.lua

--- run.task.lua
+++ run.task.lua
@@ -2,12 +2,12 @@
 print_colored("[?YW]Instalation start...[?RT]\n")
 
 print_colored("[?YW]    Installing go-build...[?RT]\n")
-run_cli("-install github.com/pt-main/run-scripts@main/go-build.py go-build 'Universal crossplatform go buildong script'")
+run_cli("-install github.com/pt-main/run-scripts@main/go-build.py go-build 'Universal crossplatform go building script'")
 
 print_colored("[?YW]    Installing addMit...[?RT]\n")
-run_cli("-install github.com/pt-main/run-scripts@main/addMit.py addMit 'Write MIT licence in current dir to LICENCE file'")
+run_cli("-install github.com/pt-main/run-scripts@main/addMit.py addMit 'Write MIT license in current dir to LICENSE file'")
 
 print_colored("[?YW]    Installing addApcache20...[?RT]\n")
-run_cli("-install github.com/pt-main/run-scripts@main/addApache20.py addApache20 'Write Apache 2.0 licence in current dir to LICENCE file'")
+run_cli("-install github.com/pt-main/run-scripts@main/addApache20.py addApache20 'Write Apache 2.0 license in current dir to LICENSE file'")
 
 print_colored("[?BGN]Instalation complete...[?RT]\n")
