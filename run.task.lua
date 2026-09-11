de958587d5814b750fb9f2256327aeddaaccbd5a
Pt <pt.main.acc@gmail.com>
2026-09-11 04:08:18 +0400

    Update run.task.lua

--- run.task.lua
+++ run.task.lua
@@ -1,3 +1,5 @@
 -- @!
 print_colored("[?YW]Instalation start...[?RT]")
+print_colored("[?YW]    Installing go-build...[?RT]")
+run_cli("-install github.com/pt-main/run-scripts@main/go-build.py")
 print_colored("[?BGN]Instalation complete...[?RT]")
