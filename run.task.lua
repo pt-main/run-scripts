4c71ab0454e0558d85f011159141bed2a8130f93
Pt <pt.main.acc@gmail.com>
2026-09-11 04:10:33 +0400

    Update run.task.lua

--- run.task.lua
+++ run.task.lua
@@ -2,6 +2,6 @@
 print_colored("[?YW]Instalation start...[?RT]\n")
 
 print_colored("[?YW]    Installing go-build...[?RT]\n")
-run_cli("-install github.com/pt-main/run-scripts@main/go-build.py")
+run_cli("-install github.com/pt-main/run-scripts@main/go-build.py go-build 'Universal crossplatform go buildong script'")
 
 print_colored("[?BGN]Instalation complete...[?RT]\n")
