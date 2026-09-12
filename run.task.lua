016b14dff00202a7029a29f1017a3e57e322c33d
Pt <pt.main.acc@gmail.com>
2026-09-12 14:06:49 +0400

    Update run.task.lua

--- run.task.lua
+++ run.task.lua
@@ -5,22 +5,33 @@ run_cli("-install github.com/pt-main/run-scripts@main/go-build.py go-build 'Univ
 print_colored("[?YW]    Installing addMit...[?RT]\n")
 run_cli("-install github.com/pt-main/run-scripts@main/addMit.py addMit 'Write MIT license in current dir to LICENSE file'")
 
-print_colored("[?YW]    Installing addApcache20...[?RT]\n")
+print_colored("[?YW]    Installing addApache20...[?RT]\n")
 run_cli("-install github.com/pt-main/run-scripts@main/addApache20.py addApache20 'Write Apache 2.0 license in current dir to LICENSE file'")
 
 -- @noreq
 print_colored("[?YW]    Installing sysinfo...[?RT]\n")
 run_cli("-install github.com/pt-main/run-scripts@main/sysinfo.lua sysinfo 'Show system info (works on windows, macos, linux)'")
 
+-- @all
+script("noreq")
+script("python")
+
 -- @
-print_colored("[?YW]Instalation start...[?RT]\n")
+print_colored("[?YW]Installation start...[?RT]\n")
 
 local args = get_args()
-for i = 1, #args do
-    local arg = args[i]
-    print_colored("[?YW]Installing " .. arg .. "...[?RT]\n")
-    script(arg)
-    print("")
+
+local ok = true
+
+if #args == 0 then
+    print_colored("[?RD]Has no args! Installation canceled[?RT]")
+    ok = false
+else
+    for i = 1, #args do
+        script(args[i])
+    end
 end
 
-print_colored("[?BGN]Instalation complete...[?RT]\n")
+if ok then 
+    print_colored("[?BGN]Installation complete...[?RT]\n")
+end
